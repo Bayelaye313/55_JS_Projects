38dad90e14cf06f0f2a7ab17511f478dac99d175
Bayelaye313 <bayelaye0510@gmail.com>
2024-01-22 23:15:45 +0000

    progress

--- P2_Progress_steps/script.js
+++ P2_Progress_steps/script.js
@@ -2,19 +2,38 @@ const btnPrev = document.querySelector('#prev');
 const btnNext = document.querySelector('#next');
 const progress = document.getElementById('progress');
 const circle = document.querySelectorAll('.circle');
+let cptActive = 1;
 
-document.addEventListener('DOMContentLoaded', function(){
-    btnNext.addEventListener('click',()=>{
-        
-        console.log(parseInt(progress.style.width));
+document.addEventListener('DOMContentLoaded', function () {
+    btnNext.addEventListener('click', () => {
+        cptActive++;
 
-        /*while (prog < '100%') {
-            prog + '38%';
-        }*/
-
+        if (cptActive <= circle.length) {
+            updateCircleClasses()
+        } else {
+            console.log('Quota atteint');
+        }
     });
-    btnPrev.addEventListener('click',()=>{
-        
-    })
 
-})
+    btnPrev.addEventListener('click', () => {
+        cptActive--;
+
+        if (cptActive >= 1) {
+            updateCircleClasses();
+        } else {
+            console.log('Déjà au début');
+        }
+    });
+
+    function updateCircleClasses() {
+        const actives = document.querySelectorAll('.active')
+            circle.forEach((item, index)=>{
+                item.classList.toggle('active', index < cptActive);
+
+            });
+            progress.style.width = (actives.length - 1) / (circle.length - 1) * 100 + '%'
+            btnPrev.disabled = cptActive === 1;
+            btnNext.disabled = cptActive === circle.length;
+    }
+
+});
